a238e1833900f37c8c6492d973968c898ffe4a11
Kaisar Ahmad Najar <kaisarnajar11114@gmail.com>
2026-06-21 07:31:00 +0530

    chore: remove unused TopBar component

--- content/hadith-quote.ts
+++
@@ -1,6 +0,0 @@
-export const hadithArabic = "خَيْرُكُمْ مَنْ تَعَلَّمَ الْقُرْآنَ وَعَلَّمَهُ";
-
-export const hadithEnglish =
-  "The best among you (Muslims) are those who learn the Qur'an and teach it.";
-
-export const hadithAttribution = "The Prophet (ﷺ) said";
